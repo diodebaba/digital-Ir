ce3ade7daf9f70bc51c4141fb4f226c7c516796e
diodebaba <222mzb@gmail.com>
2016-06-29 14:38:55 +0600

    Add files via upload

---
+++ digital_ir.ino
@@ -0,0 +1,22 @@
+int ir[4] = {9,10,11,12};
+void setup()
+{
+  for(int i = 0; i<4; i++)
+  {
+    pinMode(ir[i], INPUT);
+  }
+  Serial.begin(9600);
+}
+
+
+void loop()
+{
+  
+  for(int j = 0; j<4; j++)
+  {
+    Serial.print(digitalRead(ir[j]));
+    Serial.print(" ");
+  }
+  Serial.println(" ");
+  delay(300);
+}
